b48f578f53ca48afc16e58b1d3846814b7a215cb
Stunned1 <aidan.g124956@gmail.com>
2025-07-25 19:09:47 -0400

    added matrix functions for backpropogation; added layer class and added implementation of forward pass

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -2,15 +2,6 @@ namespace NeuralNetwork
 {
     public class NeuralNetwork
     {
-        private int[] layerSize;
-        private double[][][] weights;
-        private double[][] biases;
-        private double[][] activations;
-        private double[] derivatives;
 
-        public NeuralNetwork(int[] layerSize)
-        {
-            this.layerSize = layerSize;
-        }
     }
 }
